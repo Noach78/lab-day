9ae20fc02b1128646a5974d673a960ce536469d6
Noach78 <noachaffard@icloud.com>
2025-11-25 22:12:02 +0100

    ajout de la map + perso + deplacement

---
+++ projet labday/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip background;
+
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
