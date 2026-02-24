c27f73b327e05c8cc27aa5582395f219309a369e
Noach78 <noachaffard@icloud.com>
2026-02-25 00:56:20 +0100

    ajout menu de demarrage

---
+++ projet labday/Assets/Scripts/LancerJeu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LancerJeu : MonoBehaviour
+{
+    public void Lancer()
+    {
+        SceneManager.LoadScene("Scene"); 
+    }
+    
+}
